bb4fd118500cf1b9c75c946ec088942fbf5340f2
aga-92 <aga.jack92@gmail.com>
2022-09-17 20:41:35 +0200

    Added relevant icon from the API

--- src/script.js
+++ src/script.js
@@ -17,24 +17,29 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = date.getDay();
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
 function showTemperature(response) {
-  console.log(response);
   let currentTemperature = document.querySelector("#current-temperature");
   let city = document.querySelector("#city");
   let currentDescription = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let date = document.querySelector("#date");
+  let icon = document.querySelector("#icon");
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   currentDescription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
   date.innerHTML = formatDate(response.data.dt * 1000);
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "1028a4217ab0dffe23c1de638876457e";
