778a18623ede9f5d626493aa4b6ca1e61b4667b2
aga-92 <aga.jack92@gmail.com>
2022-09-18 11:18:39 +0200

    Added search engine

--- src/script.js
+++ src/script.js
@@ -42,7 +42,18 @@ function showTemperature(response) {
   icon.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "1028a4217ab0dffe23c1de638876457e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1028a4217ab0dffe23c1de638876457e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  search(searchInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
